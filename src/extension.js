c9c6454cc8c40b9c6ea2225d2972407eba6f9c88
khoivu <khoi.vu@nextchain.kr>
2024-11-26 15:30:12 +0700

    chore: change ext name

--- src/extension.js
+++ src/extension.js
@@ -43,11 +43,11 @@ const vscode = __importStar(require("vscode"));
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "clipbroad-log" is now active!');
+    console.log('Congratulations, your extension "clipboard-log" is now active!');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand('clipbroad-log.paste', async () => {
+    const disposable = vscode.commands.registerCommand('clipboard-log.paste', async () => {
         const editor = vscode.window.activeTextEditor;
         if (!editor) {
             vscode.window.showWarningMessage('No active editor!');
